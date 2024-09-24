bbda7a19e1b69a9e3d3069bc6da0b17dea1a7b37
fabio-pecora <fabiopecora01@gmail.com>
2024-09-24 00:26:27 -0400

    second prt of assignment not completed yet

--- first-application/first_application.py
+++ first-application/first_application.py
@@ -1,4 +1,3 @@
-
 # loading all the needed libraries
 from matplotlib import pyplot
 import numpy
@@ -16,6 +15,7 @@ filename = 'C:\\Users\\fabio\\Desktop\\glass+identification\\glass.data'
 columns = ['Id', 'Ri', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'Type']
 
 """
+
 For a better understanding of the abbreviations:
 1. Id number: 1 to 214
    2. RI: refractive index
@@ -38,6 +38,37 @@ For a better understanding of the abbreviations:
       -- 7 headlamps
 """
 
-dataset = read_csv(filename, names=columns, sep = ',')
+dataset = read_csv(filename, names=columns, sep=',')
 
-print(dataset.head())
+print("haad:" , dataset.head())
+
+print("Shape of dataset:", dataset.shape)
+
+# Set pandas display options for better output visibility
+set_option('display.max_rows', 500)
+set_option('display.width', 100)
+set_option('display.precision', 3)  # Corrected option
+
+# Print dataset types to check data types of each column
+print("Data types of each column:\n", dataset.dtypes)
+
+# Show the first 20 rows of the dataset
+print("First 20 rows of the dataset:\n", dataset.head(20))
+
+print(dataset.describe())
+
+print(dataset.groupby('Ri').size())
+
+print(dataset.corr(method = 'pearson'))
+
+print(dataset.skew())
+
+print(dataset.hist())
+pyplot.figsize = (8,8)
+pyplot.savefig('histograms.png', dpi=300)
+pyplot.show()
+
+dataset.plot(kind='density' , subplots=True, layout=(4,4), sharex=False, figsize=(14,14))
+pyplot.show()
+
+
